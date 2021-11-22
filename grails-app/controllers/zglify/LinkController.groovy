6f2839068d42e9b3ff38476317ad62b255a2d059
Ramon Oliveira <ramon.oliveira17@hotmail.com>
2021-11-22 00:57:15 -0300

    feat: add redirect route

--- grails-app/controllers/zglify/LinkController.groovy
+++ grails-app/controllers/zglify/LinkController.groovy
@@ -6,17 +6,21 @@ import org.springframework.http.HttpStatus
 
 class LinkController {
 	static responseFormats = ['json', 'xml']
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [redirect: "GET", save: "POST", update: "PUT", delete: "DELETE"]
 
     LinkService linkService
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond linkService.list(params), [linkCount:  linkService.count(), view: 'index']
-    }
+        params.offSet = 0
+        params.order = 'id'
+        params.sort = 'desc'
 
-    def show(Long id) {
-        respond linkService.get(id)
+        List<Link> links = linkService.list(params)
+        def count = linkService.count()
+        Map data = [rows: links, count: count]
+
+        respond data, view: 'index'
     }
 
     def save(Link link) {
@@ -27,37 +31,22 @@ class LinkController {
 
         try {
             linkService.register(link)
-        } catch (Exception e) {
+        } catch (ValidationException e) {
             respond link.errors, view:'create'
             return
         }
+
         respond link, [status: HttpStatus.CREATED, view:"show"]
     }
-
-    def update(Link link) {
-        if (link == null) {
-            render status: HttpStatus.NOT_FOUND
+    def redirect() {
+        String shortLink = params.shortLink
+        def originalLink = linkService.getRedirectUrl(shortLink)
+        if (originalLink == null) {
+            Map message = [message: "Não foi possível encontrar o link original"]
+            respond message, status: HttpStatus.NOT_FOUND
             return
         }
 
-        try {
-            linkService.save(link)
-        } catch (ValidationException e) {
-            respond link.errors, view:'edit'
-            return
-        }
-
-        respond link, [status: HttpStatus.OK, view:"show"]
-    }
-
-    def delete(Long id) {
-        if (id == null) {
-            render status: HttpStatus.NOT_FOUND
-            return
-        }
-
-        linkService.delete(id)
-
-        render status: HttpStatus.NO_CONTENT
+        redirect(uri: originalLink)
     }
 }
--- grails-app/services/zglify/LinkService.groovy
+++ grails-app/services/zglify/LinkService.groovy
@@ -4,30 +4,100 @@ import grails.gorm.services.Service
 import grails.gorm.transactions.Transactional
 import org.apache.commons.lang.RandomStringUtils
 
+import javax.xml.bind.ValidationException
+
 @Service(Link)
 abstract class LinkService {
 
-    abstract Link get(Serializable id)
+//    abstract Link get(Serializable id)
 
-    abstract List<Link> list(Map args)
+//    abstract List<Link> list(Map args)
 
     abstract Long count()
 
-    abstract void delete(Serializable id)
+//    abstract void delete(Serializable id)
 
-    abstract Link save(Link link)
+//    abstract Link save(Link link)
+
+    def list(Map params) {
+        try {
+            List<Link> links = Link.createCriteria().list(max: params.max, offSet: params.offSet) {
+                order(params.order, params.sort)
+            }
+            return links
+        } catch (e) {
+            throw new RuntimeException("Falha ao listar", e.getMessage())
+        }
+    }
+
+    def get(Serializable id) {
+        try {
+            Link link = Link.findById(id)
+            return link
+        } catch (e) {
+            throw new RuntimeException("Falha ao buscar registro", e.getMessage())
+        }
+    }
+
+    def save(Link link) {
+        Link.withTransaction { status ->
+            try {
+                link.save(flush: true, failOnError: true)
+            } catch (ValidationException e) {
+                status.setRollbackOnly()
+                throw new RuntimeException("Falha ao salvar", e.getMessage())
+            }
+        }
+    }
+
+    def update(Link link) {
+        Link.withTransaction { status ->
+            try {
+                link.save(flush: true, failOnError: true)
+            } catch (ValidationException e) {
+                status.setRollbackOnly()
+                throw new RuntimeException("Falha ao atualizar", e.getMessage())
+            }
+        }
+    }
+
+    def delete(Serializable id) {
+        Link.withTransaction { status ->
+            try {
+                def link = Link.findById(id)
+                link.delete(flush: true, failOnError: true)
+
+            } catch (Exception e) {
+                throw new RuntimeException("Falha ao deletar", e.getMessage())
+            }
+        }
+    }
 
     @Transactional
     Link register(Link link) {
+        if (!link.originalLink.contains('http://') || !link.originalLink.contains('https://')) {
+            link.originalLink = 'https://' + link.originalLink
+        }
         link.shortLink = generateShortLink()
         save(link)
+
         return link
     }
 
+    String getRedirectUrl(String shortLink) {
+        Link link = Link.createCriteria().get {
+            eq('shortLink', shortLink)
+        }
+
+        return link?.originalLink
+
+    }
+
     private String generateShortLink() {
-        def baseUrl = "http://localhost:8080/"
-        def randomString = RandomStringUtils.randomAlphanumeric(5)
-        return baseUrl + randomString
+        def randomUrlLength = 5
+        def randomShortUrl = RandomStringUtils.randomAlphanumeric(randomUrlLength)
+
+        return randomShortUrl
     }
 
 }
